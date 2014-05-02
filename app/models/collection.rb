e9aa172ea89835dcd1907e78585211e61a3aa207
Adam Crownoble <adam.crownoble@biola.edu>
2014-05-01 17:07:49 -0700

    Friendly URLs for collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,6 @@
 class Collection
   include Mongoid::Document
+  include Mongoid::Slug
 
   has_and_belongs_to_many :products
   field :name, type: String
@@ -7,6 +8,8 @@ class Collection
   field :layout, type: Symbol
 
   validates :name, presence: true, uniqueness: true
+
+  slug :name
 
   alias :to_s :name
 end
