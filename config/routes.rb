7938199935f8169d803c5c37a02ae9767507b9d2
Adam Crownoble <adam.crownoble@biola.edu>
2014-05-19 14:32:09 -0700

    Add logout link

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   # for TouchNet post-backs
   post '/payment', to: 'payments#update'
 
+  # this is just a convenience to create a named route to rack-cas' logout
+  get '/logout' => -> env { [200, { 'Content-Type' => 'text/html' }, ['Rack::CAS should have caught this']] }, as: :logout
+
   root 'offerings#index'
 end
