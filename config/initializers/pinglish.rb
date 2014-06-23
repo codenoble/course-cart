d81d140e69437bf5208f8b5d66ad81101bcb860d
Adam Crownoble <adam.crownoble@biola.edu>
2014-06-22 23:06:24 -0700

    Add pinglish for uptime monitoring

---
+++ config/initializers/pinglish.rb
@@ -0,0 +1,23 @@
+require 'oci8'
+
+CourseCart::Application.config.middleware.use Pinglish do |ping|
+  ping.check :mongodb do
+    Mongoid.default_session.command(ping: 1).has_key? 'ok'
+  end
+
+  ping.check :banner do
+    conn = OCI8.new(Settings.banner.connection_string)
+    conn.ping.tap do
+      conn.logoff
+    end
+  end
+
+  # ping.check :smtp do
+  #   smtp = Net::SMTP.new(ActionMailer::Base.smtp_settings[:address])
+  #   smtp.start
+  #   ok = smtp.started?
+  #   smtp.finish
+
+  #   ok
+  # end
+end
